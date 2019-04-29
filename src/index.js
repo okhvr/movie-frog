eaab24f1e2582e1e81b94f8686afea52dd3ef87c
Olha Katsalap <olg.katsalap@gmail.com>
2019-04-29 19:13:45 +0300

    persist store

--- src/index.js
+++ src/index.js
@@ -4,21 +4,35 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';  
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './style.scss';
 
+const persistConfig = {
+    key: 'root',
+    storage,
+  };
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = createStore(
-    rootReducer,
+    persistedReducer,
     applyMiddleware(thunk)
 );
 
+const persistor = persistStore(store);
+
 ReactDOM.render(
     <Provider store={store}>
-        <main className="scene">
-            <App/>
-        </main>
+        <PersistGate loading={null} persistor={persistor}>
+            <main className="scene">
+                <App/>
+            </main>
+        </PersistGate>
     </Provider>,
     document.getElementById('movie-app')
 );
